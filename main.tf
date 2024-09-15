b6f158b30daf204e3fbf8a9fd5e886100cade460
Olivier Hayot <o.hayot@wavenet.be>
2024-09-15 18:09:07 +0200

    changed source for tests

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "postgres1" {
-  source        = "./modules/postgres"
+  source        = "github.com/h0livier/terraform-docker-modules//modules/postgres"
 
   name_prefix = "postgres1"
   postgres_config = var.postgres_config
 }
 
 module "mysql1" {
-  source        = "./modules/mysql"
+  source        = "github.com/h0livier/terraform-docker-modules//modules/mysql"
 
   name_prefix = "mysql1"
   config = var.mysql_config
